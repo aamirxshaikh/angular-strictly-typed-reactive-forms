a922e9751c9042f513c6bb249b701bd3cc0136a5
Aamir shaikh <aamirshaikh3232@outlook.com>
2023-02-06 00:36:25 +0530

    Update CustomFormValidatorService

--- src/app/services/custom-form-validator.service.ts
+++ src/app/services/custom-form-validator.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomFormValidatorService {
+  passwordPatternValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) {
+        return null;
+      }
+      const regex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+      const validPattern = regex.test(control.value);
+      return validPattern ? null : { invalidPassword: true };
+    };
+  }
 
-  constructor() { }
+  matchPasswordValidator(password: string, confirmPassword: string) {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const passwordControl = formGroup.get(password);
+      const confirmPasswordControl = formGroup.get(confirmPassword);
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors['passwordMismatch']
+      ) {
+        return null;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+      }
+      else {
+        confirmPasswordControl.setErrors(null);
+        return null;
+      }
+    };
+  }
 }
